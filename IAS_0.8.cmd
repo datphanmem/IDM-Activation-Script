5e6a277ff5671f51754ac0756ed15b4531dc97b2
Đạt Phần Mềm <68048078+datphanmem@users.noreply.github.com>
2023-07-30 21:45:42 +0900

    Update IAS_0.8.cmd

--- IAS_0.8.cmd
+++ IAS_0.8.cmd
@@ -153,7 +153,7 @@ if defined activate goto _activate
 :MainMenu
 
 cls
-title  IDM Activation Script 0.7 - @ModByPiash
+title  IDM Activation Script 0.7 - @DatPhanMem
 mode 65, 25
 
 :: Check firewall status
